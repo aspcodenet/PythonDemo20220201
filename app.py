75b13f40c6718158cf456e86e464fb53e3c13e02
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-02-01 09:29:22 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,205 @@
+from flask import Flask, render_template, request, url_for, redirect
+from models import db, Person, seedData,UserRegistration
+from flask_migrate import Migrate, upgrade
+from random import randint
+from forms import PersonEditForm, PersonNewForm, UserRegistrationForm
+from models import User, user_manager
+from flask_user import login_required, roles_required, roles_accepted
+
+app = Flask(__name__)
+app.config.from_object('config.ConfigDebug')
+
+db.app = app
+db.init_app(app)
+migrate = Migrate(app,db)
+user_manager.app = app
+user_manager.init_app(app,db,User)
+
+
+@app.route("/hej")
+def hejPage():
+    # på riktigt kolla om inloggad osv sov
+    lista = ["Stefan", "Oliver", "Josefine"]
+    return render_template('hej.html', inloggad=True, lista=lista,age=49, name="Stefan")
+
+
+
+@app.route("/")
+def indexPage():
+    activePage = "startPage"
+    allaPersoner = Person.query.all()
+    #antalPersoner = Person count???
+    #totSaldo = 
+    return render_template('startPage.html', antalPersoner=12, totSaldo=999,activePage=activePage)
+
+# To improve: get med defaultvalue
+# search!
+@app.route("/personer")
+#@login_required
+# @roles_accepted('Customer', 'Admin') # AND # OR
+def personerPage():
+    
+    sortColumn = request.args.get('sortColumn', 'namn')
+    sortOrder = request.args.get('sortOrder', 'asc')
+    page = int(request.args.get('page', 1))
+
+    searchWord = request.args.get('q','')
+
+    activePage = "personerPage"
+    allaPersoner = Person.query.filter(
+        Person.namn.like('%' + searchWord + '%') | 
+        Person.city.like('%' + searchWord + '%')  | 
+        Person.id.like(searchWord)          )
+
+    if sortColumn == "namn":
+        if sortOrder == "desc":
+            allaPersoner = allaPersoner.order_by(Person.namn.desc())
+        else:
+            allaPersoner = allaPersoner.order_by(Person.namn.asc())
+
+    if sortColumn == "city":
+        if sortOrder == "desc":
+            allaPersoner = allaPersoner.order_by(Person.city.desc())
+        else:
+            allaPersoner = allaPersoner.order_by(Person.city.asc())
+
+    if sortColumn == "postal":
+        if sortOrder == "desc":
+            allaPersoner = allaPersoner.order_by(Person.postalcode.desc())
+        else:
+            allaPersoner = allaPersoner.order_by(Person.postalcode.asc())
+
+    paginationObject = allaPersoner.paginate(page,20,False)
+
+
+    return render_template('personer.html', 
+            allaPersoner=paginationObject.items, 
+            page=page,
+            sortColumn=sortColumn,
+            sortOrder=sortOrder,
+            q=searchWord,
+            has_next=paginationObject.has_next,
+            has_prev=paginationObject.has_prev, 
+            pages=paginationObject.pages, 
+            activePage=activePage)
+
+@app.route("/userconfirmation")
+def userConfirmationPage():
+    namnet = request.args.get('namn',"")
+    return render_template('userconfirmation.html',namn=namnet)
+
+@app.route("/newuser",methods=["GET", "POST"]) 
+def userRegistrationPage():
+    form = UserRegistrationForm(request.form) 
+    if request.method == "GET":
+        return render_template('userregistration.html',form=form)
+    if form.validate_on_submit():
+        userReg = UserRegistration()
+        userReg.email = form.email.data
+        userReg.firstname = form.firstname.data
+        userReg.lastname = form.lastname.data
+        userReg.password = form.pwd.data
+        userReg.updates = form.updates.data
+        db.session.add(userReg)
+        db.session.commit()
+        return redirect(url_for('userConfirmationPage', namn=form.firstname.data ))
+
+    return render_template('userregistration.html',form=form)
+
+
+
+
+@app.route("/personnew",methods=["GET", "POST"]) 
+# @roles_required("Admin")
+def personNewPage():
+    form = PersonNewForm(request.form) 
+
+    if request.method == "GET":
+        return render_template('personnew.html',form=form)
+
+    if form.validate_on_submit():
+        personFromDb = Person()
+        personFromDb.namn = form.name.data
+        personFromDb.city = form.city.data 
+        personFromDb.postalcode = str(form.postalcode.data)
+        personFromDb.position = form.position.data
+        db.session.add(personFromDb)
+        db.session.commit()
+        return redirect(url_for('personerPage'))
+
+    return render_template('personnew.html',form=form)
+
+
+@app.route("/person/<id>",methods=["GET", "POST"])  # EDIT   3
+# @roles_required("Admin")
+def personPage(id):
+    form = PersonEditForm(request.form) 
+    personFromDb = Person.query.filter(Person.id == id).first()
+
+    if request.method == "GET":
+        form.name.data = personFromDb.namn
+        form.city.data = personFromDb.city
+        form.postalcode.data = int(personFromDb.postalcode)
+        form.position.data = personFromDb.position
+        return render_template('person.html',person=personFromDb, form=form)
+    if form.validate_on_submit():
+        personFromDb.namn = form.name.data
+        personFromDb.city = form.city.data 
+        personFromDb.position = form.position.data
+        personFromDb.postalcode = str(form.postalcode.data)
+        db.session.commit()
+        return redirect(url_for('personerPage'))
+    return render_template('person.html',person=personFromDb, form=form)
+    
+
+
+
+
+@app.route("/hopp")
+def hoppPage():
+    return "<html><body><h1>Hopp</h1></body></html>"
+
+@app.route("/personer2")
+def personerPage2():
+    
+    sortColumn = request.args.get('sortColumn',"namn")
+    sortOrder = request.args.get('sortOrder', "asc")
+    page = request.args.get('page', 1, type=int)
+
+    activePage = "personerPage"
+    allaPersoner = Person.query
+
+
+    if sortColumn == "namn":
+        if sortOrder == "desc":
+            allaPersoner = allaPersoner.order_by(Person.namn.desc())
+        else:
+            allaPersoner = allaPersoner.order_by(Person.namn.asc())
+
+    if sortColumn == "city":
+        if sortOrder == "desc":
+            allaPersoner = allaPersoner.order_by(Person.city.desc())
+        else:
+            allaPersoner = allaPersoner.order_by(Person.city.asc())
+
+    if sortColumn == "postal":
+        if sortOrder == "desc":
+            allaPersoner = allaPersoner.order_by(Person.postalcode.desc())
+        else:
+            allaPersoner = allaPersoner.order_by(Person.postalcode.asc())
+
+    paginationObject = allaPersoner.paginate(page,20,False)
+
+    return render_template('personer.html', 
+                    allaPersoner=paginationObject.items, 
+            activePage=activePage)
+
+
+
+if __name__  == "__main__":
+    with app.app_context():
+        upgrade()
+        seedData()
+    app.run()
+
+
---
+++ models.py
@@ -0,0 +1,123 @@
+from flask_sqlalchemy import SQLAlchemy
+import barnum
+from flask_user import  UserMixin, UserManager
+from datetime import datetime
+import random
+
+db = SQLAlchemy()
+
+
+class Person(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    namn = db.Column(db.String(80), unique=False, nullable=False)
+    city = db.Column(db.String(80), unique=False, nullable=False)
+    postalcode = db.Column(db.String(10), unique=False, nullable=False)
+    position = db.Column(db.String(1), unique=False, nullable=False) # G, D, F
+    cards = db.relationship('CreditCard', backref='Person',lazy=True)
+
+class UserRegistration(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(80), unique=False, nullable=False)
+    firstname = db.Column(db.String(40), unique=False, nullable=False)
+    lastname = db.Column(db.String(40), unique=False, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    updates = db.Column(db.Boolean, unique=False, nullable=False)  
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    active = db.Column('is_active', db.Boolean(), nullable=False, server_default='1')
+
+    # User authentication information. The collation='NOCASE' is required
+    # to search case insensitively when USER_IFIND_MODE is 'nocase_collation'.
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    email_confirmed_at = db.Column(db.DateTime()) 
+
+    password = db.Column(db.String(255), nullable=False, server_default='')
+
+    # User information
+    first_name = db.Column(db.String(100), nullable=False, server_default='')
+    last_name = db.Column(db.String(100), nullable=False, server_default='')
+
+    # Define the relationship to Role via UserRoles
+    roles = db.relationship('Role', secondary='user_roles')
+
+# Define the Role data-model
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+
+# Define the UserRoles association table
+class UserRoles(db.Model):
+    __tablename__ = 'user_roles'
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey('users.id', ondelete='CASCADE'))
+    role_id = db.Column(db.Integer(), db.ForeignKey('roles.id', ondelete='CASCADE'))
+
+class CreditCard(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    cardtype = db.Column(db.String(30), unique=False, nullable=False)
+    number = db.Column(db.String(30), unique=False, nullable=False)
+    PersonId = db.Column(db.Integer, db.ForeignKey('person.id'), nullable=False)
+    Datum = db.Column(db.DateTime, unique=False, nullable=False)
+
+
+user_manager = UserManager(None, db, User) 
+
+
+def seedData():
+    AddRoleIfNotExists("Admin")
+    AddRoleIfNotExists("Customer")
+    AddLoginIfNotExists("admin@example.com", "Hejsan123#",["Admin"])
+    AddLoginIfNotExists("customer@example.com", "Hejsan123#",["Customer"])
+
+    antal =  Person.query.count()
+    while antal < 100:
+        person = Person()
+        person.postalcode, person.city, _  = barnum.create_city_state_zip()
+        namn1, namn2 = barnum.create_name()
+        person.namn = namn1 + " " + namn2
+        person.position = "G"
+        antal = antal + 1
+        db.session.add(person)
+        db.session.commit()        
+    antal =  CreditCard.query.count()   
+    if antal > 100:
+        return
+    for person in Person.query.all():
+        for x in range(3,random.randint(3, 30)):
+            namn,number = barnum.create_cc_number()
+            c = CreditCard()
+            c.cardtype = namn
+            c.number = number[0]
+            c.Datum = barnum.create_date(past=True)
+            person.cards.append(c)
+        db.session.commit()
+
+
+
+
+def AddRoleIfNotExists(namn:str): 
+    if Role.query.filter(Role.name == namn).first():
+        return
+    role = Role()
+    role.name = namn
+    db.session.add(role)
+    db.session.commit()
+
+
+def AddLoginIfNotExists(email:str, passwd:str, roles:list[str]):
+    if User.query.filter(User.email == email).first():
+        return
+    user = User()
+    user.email=email
+    user.email_confirmed_at=datetime.utcnow()
+    user.password=user_manager.hash_password(passwd)    
+    for roleName in roles:
+        role = Role.query.filter(Role.name == roleName).first()
+        user.roles.append(role)
+
+    db.session.add(user)
+    db.session.commit()
